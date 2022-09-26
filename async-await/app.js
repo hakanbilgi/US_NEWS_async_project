3039f8e7535a26f26f03a4e008b86a33715d881d
Hakan Bilgi <hknblg34@gmail.com>
2022-09-26 11:28:41 +0300

    await

--- async-await/app.js
+++ async-await/app.js
@@ -1,10 +1,10 @@
+const getNews = async function () {
+  const API_KEY = "cd8a8d77a8b24209927ac807383adf61";
+  const url =
+    "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
 
-
-const getNews = async function(){
-   const API_KEY = "cd8a8d77a8b24209927ac807383adf61"; 
-const url = "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
-
-const res = await fetch(url)
-console.log(res);
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.articles);
 };
 getNews();
